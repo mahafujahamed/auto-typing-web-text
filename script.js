3cae851a2dd09ba867ea605086e4b4f6ffed9798
Mahafuj-Ahamed <mahafujahamed068@gamil.com>
2023-08-30 20:47:25 +0600

    impliment auto typing text any website

---
+++ script.js
@@ -0,0 +1,44 @@
+// one time animation
+
+// const textElement = document.getElementById('typing-text');
+// const textToType = 'Hello, welcome to my website!';
+// let index = 0;
+
+// function typeText() {
+//     if (index < textToType.length) {
+//         textElement.textContent += textToType.charAt(index);
+//         index++;
+//         setTimeout(typeText, 100); // Adjust typing speed here (in milliseconds)
+//     }
+// }
+
+// typeText();
+
+// continue animation
+
+const textElement = document.getElementById('typing-text');
+const textToType = 'Hello, welcome to my website!';
+let index = 0;
+
+function typeText() {
+    if (index < textToType.length) {
+        textElement.textContent += textToType.charAt(index);
+        index++;
+        setTimeout(typeText, 100); // Adjust typing speed here (in milliseconds)
+    } else {
+        setTimeout(eraseText, 1000); // Delay before erasing text (in milliseconds)
+    }
+}
+
+function eraseText() {
+    if (index > 0) {
+        textElement.textContent = textToType.substring(0, index - 1);
+        index--;
+        setTimeout(eraseText, 50); // Adjust erasing speed here (in milliseconds)
+    } else {
+        index = 0;
+        setTimeout(typeText, 1000); // Delay before starting to type again (in milliseconds)
+    }
+}
+
+typeText();
